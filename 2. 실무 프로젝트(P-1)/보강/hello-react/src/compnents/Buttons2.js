621a8f2497196235b91a2f1b1999d645de7d0ff4
soeunl <soeun318@gmail.com>
2024-07-24 19:20:57 +0900

    리액트보강

--- 2. 실무 프로젝트(P-1)/보강/hello-react/src/compnents/Buttons2.js
+++ 2. 실무 프로젝트(P-1)/보강/hello-react/src/compnents/Buttons2.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 class Buttons2 extends Component {
   render() {
-    const {color, children} = this.props; // 비구조화할당을 한다!
+    const { color, children: c } = this.props; // 비구조화할당을 한다!
     // 값을 분해해서 사용한다
 
     console.log('props', Object.getOwnPropertyDescriptor(this.props));
-    console.log('props')
+    console.log('props');
 
     const styles = {
       background: color,
     };
 
-    return <button style={styles}>{children}</button>;
+    return <button style={styles}>{c}</button>;
     // 소괄호 안쪽이 모두 JSX 문법이 들어간다.
   }
 }
