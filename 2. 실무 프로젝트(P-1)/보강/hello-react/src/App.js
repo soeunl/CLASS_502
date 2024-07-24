3dffc035e35867a2829fcbc8e8cfa6685d763f99
soeunl <soeun318@gmail.com>
2024-07-24 09:07:47 +0900

    리액트 보강

--- 2. 실무 프로젝트(P-1)/보강/hello-react/src/App.js
+++ 2. 실무 프로젝트(P-1)/보강/hello-react/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Buttons from "./compnents/Buttons";
 
 function App() {
+  const name = '이소은';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          리액트~~
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안녕하세용
-        </a>
-      </header>
-    </div>
+    // 이 안에 출력할 내용을 정의한다.
+    // 안에 출력할 내용이 하나일때는 소괄호 생략가능하지만,
+    // 하나 이상일때는 소괄호를 생략할 수 없다.
+    <>
+      {name && <h1>안녕하세요👧, {name}</h1>}
+      <h2>반갑습니다🧑</h2>
+      <Buttons />
+    </>
   );
 }
 
---
+++ 2. 실무 프로젝트(P-1)/보강/hello-react/src/compnents/Buttons.js
@@ -0,0 +1,10 @@
+function Buttons() {
+
+  const handleClick = () => {
+    window.alert('버튼이 클릭되었습니다!');
+  };
+
+  return <button onClick={handleClick}>클릭</button>;
+}
+
+export default Buttons;
