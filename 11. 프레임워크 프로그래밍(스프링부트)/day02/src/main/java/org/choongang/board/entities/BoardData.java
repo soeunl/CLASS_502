a4dd1ce47dfbc244979f47020a411f13acd80cd5
soeunl <soeun318@gmail.com>
2024-07-27 12:23:46 +0900

    코드 분석

--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/board/entities/BoardData.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/board/entities/BoardData.java
@@ -10,24 +10,24 @@ import org.choongang.member.entities.Member;
 
 @Data
 @Builder
-@Entity
+@Entity // JPA의 어노테이션으로, 이 클래스가 데이터베이스의 테이블과 매핑됨을 나타냄
 @AllArgsConstructor // 클래스의 모든 필드를 사용하여 생성자를 생성
 @NoArgsConstructor // 매개변수가 없는 기본 생성자를 생성
 // 이 두 애노테이션을 함께 사용하면, 클래스에 모든 필드를 매개변수로 받는 생성자와 매개변수가 없는 기본 생성자가 모두 자동으로 생성되어 편리하게 사용할 수 있음
 public class BoardData extends BaseEntity {
-    @Id @GeneratedValue
+    @Id @GeneratedValue // 엔티티의 기본 키로 사용되며, 데이터베이스에서 자동으로 생성되는 값
     private Long seq;
 
-    @ManyToOne(fetch=FetchType.LAZY)
+    @ManyToOne(fetch=FetchType.LAZY) // 엔티티(Member)와의 다대일 관계 / 지연로딩
     // many가 있는 쪽에 정의한다
     // member_seq (외래키가 엔티티명 기준으로 만들어진다) 엔티티명_기본키 속성명
-    @JoinColumn(name = "mSeq") // 컬럼명 바꿀때 사용
+    @JoinColumn(name = "mSeq") // 컬럼명 바꿀때 사용 / Member 엔티티와의 관계를 위한 외래 키 컬럼 이름을 지정
     private Member member;
 
     @Column(nullable = false) // null은 안되고 크기는 255로 한다.
     private String subject;
 
-    @Lob
+    @Lob // 대용량 데이터를 저장하기 위한 LOB(Large Object) 타입
     private String content;
 
 //    @ManyToMany
--- 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/board/repositories/BoardDataRepository.java
+++ 11. 프레임워크 프로그래밍(스프링부트)/day02/src/main/java/org/choongang/board/repositories/BoardDataRepository.java
@@ -11,15 +11,22 @@ import java.util.List;
 public interface BoardDataRepository extends JpaRepository<BoardData, Long>, QuerydslPredicateExecutor<BoardData> {
     // 자바 표준 프록시..?
     // 인터페이스로 만들어야 한다
+    // 스프링 데이터 JPA는 레포지토리 인터페이스를 보고 동적으로 프록시 객체를 생성함. 이 프록시 객체는 실제 데이터베이스와 상호작용하는 역할을 하며, 메소드 이름을 기반으로 SQL 쿼리를 생성함
+    // JPA 레포지토리 인터페이스를 상속함으로써 기본적인 CRUD(Create, Read, Update, Delete) 기능을 제공
+    // <BoardData, Long>은 이 레포지토리가 BoardData 엔티티를 관리하며, 기본 키(Primary Key)의 타입이 Long임을 나타냄
+    // QuerydslPredicateExecutor 인터페이스를 상속함으로써 Querydsl 라이브러리를 활용하여 동적인 쿼리 작성 가능
 
     // JPQL
     @Query("SELECT b FROM BoardData b LEFT JOIN FETCH b.member")
-    List<BoardData> getAllList(); // 이렇게 해두어도 엔티티에 정의한 매핑 전략으로 쿼리가 실행된다,,,?
+    List<BoardData> getAllList();
+    // 이렇게 해두어도 엔티티에 정의한 매핑 전략으로 쿼리가 실행된다,,,?
     // 기본적으로 엔티티에 정의된 전략대로 쿼리가 수행된다.
     // FETCH를 쓰면 처음부터 조인이 되어서 시작된다.
 
     @EntityGraph(attributePaths = "member") // 처음부터 패치가 된다
     //바로 조회 할 엔티티 명시
         // Board Data의 member 엔티티, 배열 형태가 많으면 중괄호 형태로 입력
+    // 쿼리 실행 시 member 필드와 연관된 엔티티(게시글 작성자 정보)를 함께 가져오도록 지시
     List<BoardData> findBySubjectContaining(String key);
+    // 메서드 매개변수로 String key를 받아 제목(subject)에 key 문자열이 포함된 게시글 목록을 조회
 }
