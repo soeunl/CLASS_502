2862cec858d116c8533d8fbda2adb2063101eb2b
soeunl <soeun318@gmail.com>
2024-09-06 20:44:36 +0900

    9월 6일 수업

---
+++ 16. 리액트 네이티브/exam01/src/app/counter2/Counter.js
@@ -0,0 +1,12 @@
+'use client';
+import React, { useReducer } from 'react';
+
+const Counter = () => {
+  return (
+    <>
+      <h1>0</h1>
+    </>
+  );
+};
+
+export default Counter;
---
+++ 16. 리액트 네이티브/exam01/src/app/counter2/page.js
@@ -0,0 +1,11 @@
+import Counter from '../counter/page';
+
+export const metadata = {
+  title: '카운터',
+};
+
+const CounterPage = () => {
+  return <Counter />;
+};
+
+export default CounterPage;
